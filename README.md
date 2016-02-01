3b18348e2b91e447c634428b8343d0fec0b5b73c
Ce Gao <gaocegege@hotmail.com>
2016-02-01 12:58:51 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+resume
+======
+
+[View it here](http://gaocegege.com/resume/)
+
+Print the page as PDF to get a copy.
+
+
---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+hackmyresume BUILD ./resume-jrs.json TO ./index.html -t ./node_modules/jsonresume-theme-onepage
