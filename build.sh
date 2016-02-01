dcd75a242d6ee79fe8f165d0afd58bef42622a08
Ce Gao <gaocegege@hotmail.com>
2016-02-01 13:19:03 +0800

    add the cn version

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 hackmyresume BUILD ./resume-jrs.json TO ./index.html -t ./node_modules/jsonresume-theme-onepage
+hackmyresume BUILD ./cn/resume-jrs.json TO ./cn/index.html -t ./node_modules/jsonresume-theme-onepage
+
 open ./index.html
+open ./cn/index.html
